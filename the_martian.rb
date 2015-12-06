900893c83c88f94a7db9089742158a14378202a5
Nicky Peeters <nicky@speakermouth.com>
2015-12-06 16:51:19 +0100

    quick-and-dirty running of the example input

---
+++ the_martian.rb
@@ -0,0 +1,39 @@
+require_relative('./lib/rover.rb')
+
+def science_the_shit_out_of_this
+  input = <<-INPUT
+5 5
+
+1 2 N
+
+LMLMLMLMM
+
+3 3 E
+
+MMRMMRMRRM
+  INPUT
+
+  io = StringIO.new(input)
+  rover1, rover2 = nil, nil
+
+  io.each_line do |line|
+    # skipping first two until we've modeled the grid
+    line = line.strip
+    rover1 = Rover.land(line) if io.lineno == 3
+    rover1.command(line) if io.lineno == 5
+    rover2 = Rover.land(line) if io.lineno == 7
+    rover2.command(line) if io.lineno == 9
+
+  end
+
+  puts rover1.to_s
+  puts
+  puts rover2.to_s
+
+
+end
+
+science_the_shit_out_of_this
+
+
+
