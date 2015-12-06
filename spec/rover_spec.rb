f2d223f010ac68aa552a818cd237d94b16e69c9a
Nicky Peeters <nicky@speakermouth.com>
2015-12-06 14:48:07 +0100

    . for class methods, new for how it’s used

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -3,7 +3,7 @@ require 'rover'
 
 describe Rover do
 
-  describe '#initialize' do
+  describe '.new' do
     it 'creates a rover' do
       r = Rover.new(x: 1, y: 1, heading: "S")
       expect(r).to be_a_kind_of(Rover)
