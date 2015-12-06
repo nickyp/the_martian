884d985dcaa5e6d4dc7aa16b41a44466c7f4450d
Nicky Peeters <nicky@speakermouth.com>
2015-12-06 14:30:20 +0100

    only accept valid headings

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,9 +1,12 @@
 class Rover
+  COMPASS = %w{N E S W}
 
   attr_accessor :x, :y, :heading
 
   def initialize(x: 0, y: 0, heading: "N")
     @x = x, @y = y
+
+    raise ArgumentError.new("Invalid heading: #{heading}") unless COMPASS.include?(heading.upcase)
     @heading = heading
   end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -8,5 +8,15 @@ describe Rover do
       r = Rover.new(x: 1, y: 1, heading: "S")
       expect(r).to be_a_kind_of(Rover)
     end
+
+    it 'raises an ArgumentError for invalid headings' do
+      expect { Rover.new(x: 1, y: 1, heading: "SX") }.to raise_error(ArgumentError)
+    end
+
+    it 'but accepts headings in downcase' do
+      r = Rover.new(x: 1, y: 1, heading: "s")
+      expect(r).to be_a_kind_of(Rover)
+    end
+
   end
 end
